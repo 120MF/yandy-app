0be8a2a01fc5ed538b586d78a12e703290b1cc97
WindWeaver <moonbite233@gmail.com>
2026-01-08 15:37:22 +0800

    configs OneChassisNode and ControllerHub

--- app/src/configs.hpp
+++ app/src/configs.hpp
@@ -1,11 +1,14 @@
 #pragma once
+#include "OneChassisNode.hpp"
 
 // 在这里添加各个Hub的编译期Config
 // 你可以在 one-framework 文档中查找到各个Hub所需的配置
-
+constexpr ControllerHubConfig controller_hub_config{
+    .input_device = DEVICE_DT_GET_ANY(dji_dbus)
+};
 
 // 在这里配置 Nodes
-void config_nodes ()
+void config_nodes()
 {
-
+    OneChassisNode::bind<OneChassisNode::Config>({DEVICE_DT_GET(DT_NODELABEL(can1))});
 }
--- app/src/main.cpp
+++ app/src/main.cpp
@@ -9,12 +9,18 @@ LOG_MODULE_REGISTER(main, CONFIG_LOG_DEFAULT_LEVEL);
 
 using namespace OF;
 
-int main() {
-  LOG_INF("main");
-  HubRegistry::startAll();
-  config_nodes();
-  start_all_nodes();
-  while (true) {
-    k_sleep(K_MSEC(1000));
-  }
+int main()
+{
+    LOG_INF("main");
+    HubRegistry::startAll();
+    config_nodes();
+    start_all_nodes();
+    while (true)
+    {
+        k_sleep(K_MSEC(1000));
+    }
 }
+
+//TODO:
+// 1. 目前add-node添加的路径还是不对，create-node功能没改
+// 2. 使用Github模板会把module.yml里的self path带过来，导致所有的目录名都叫one-starter，要不改叫one-app？
