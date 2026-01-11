055df7a73cf3b3fdf5e622734ae211bea6dbb96e
WindWeaver <moonbite233@gmail.com>
2026-01-11 11:48:21 +0800

    config notifyhub

--- app/src/configs.hpp
+++ app/src/configs.hpp
@@ -1,11 +1,23 @@
 #pragma once
-#include "OF/lib/ControllerHub/ControllerHub.hpp"
+#include <OF/lib/ControllerHub/ControllerHub.hpp>
+#include <OF/lib/ImuHub/ImuHub.hpp>
+#include <OF/lib/NotifyHub/NotifyHub.hpp>
 #include "OneChassisNode.hpp"
 
 // 在这里添加各个Hub的编译期Config
 // 你可以在 one-framework 文档中查找到各个Hub所需的配置
-constexpr OF::ControllerHubConfig controller_hub_config{
-    .input_device = DEVICE_DT_GET_ANY(dji_dbus)
+constexpr ControllerHubConfig controller_hub_config{
+    .input_device = DEVICE_DT_GET(DT_NODELABEL(usart3))
+};
+
+constexpr ImuHubConfig imu_hub_config{
+    .accel_dev = DEVICE_DT_GET(DT_NODELABEL(bmi088_accel)),
+    .gyro_dev = DEVICE_DT_GET(DT_NODELABEL(bmi088_gyro))
+};
+
+constexpr NotifyHubConfig notify_hub_config{
+    .led_pixel_dev = DEVICE_DT_GET(DT_NODELABEL(pixel_led)),
+    .pwm_buzzer_dev = DEVICE_DT_GET(DT_NODELABEL(pwm_buzzer))
 };
 
 // 在这里配置 Nodes
--- app/src/main.cpp
+++ app/src/main.cpp
@@ -22,10 +22,13 @@ int main()
     {
         const auto data = topic_one_chassis.read();
         LOG_INF("%s", data.format().c_str());
+        auto [quat, euler_angle, gyro, accel] = getImuData();
+        auto& [p, r, y] = euler_angle;
+        LOG_INF("euler: %f, %f, %f;", p, r, y);
         k_sleep(K_MSEC(300));
     }
 }
 
 //TODO:
-// 1. 目前add-node添加的路径还是不对，create-node功能没改
+// 1. 目前add-node添加的路径还是不对，create-node功能没改 (solved)
 // 2. 使用Github模板会把module.yml里的self path带过来，导致所有的目录名都叫one-starter，要不改叫one-app？
