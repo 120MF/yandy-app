7139aa1d7c1e0ed1603c610627874c0ac1588722
WindWeaver <moonbite233@gmail.com>
2026-03-20 06:12:38 +0800

    add servo configs

--- app/src/configs.hpp
+++ app/src/configs.hpp
@@ -1,8 +1,34 @@
 #pragma once
 #include "OneChassisNode.hpp"
+#include <YandyGimbalNode.hpp>
+#include <zephyr/drivers/pwm.h>
+
+// Servo PWM specs - TIM1 CH1 and CH2, 20ms period (50Hz)
+// Channel numbering: TIM1_CH1 = 1, TIM1_CH2 = 2
+// The PWM device is the pwm child node itself, not its parent (timers)
+static constexpr pwm_dt_spec servo1_spec = {
+    .dev = DEVICE_DT_GET(DT_NODELABEL(pwm1)),
+    .channel = 1,
+    .period = PWM_MSEC(20),
+    .flags = PWM_POLARITY_NORMAL
+};
+
+static constexpr pwm_dt_spec servo2_spec = {
+    .dev = DEVICE_DT_GET(DT_NODELABEL(pwm1)),
+    .channel = 2,
+    .period = PWM_MSEC(20),
+    .flags = PWM_POLARITY_NORMAL
+};
 
 // 在这里配置 Nodes
 void config_nodes()
 {
     OneChassisNode::bind<OneChassisNode::Config>({DEVICE_DT_GET(DT_NODELABEL(can1))});
+    
+    YandyGimbalNode::bind<YandyGimbalNode::Config>({
+        DEVICE_DT_GET(DT_NODELABEL(can1)),
+        &servo1_spec,
+        &servo2_spec
+    });
 }
+
